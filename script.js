4eabcfb8572b790f935a3dee7bd6f595d45a05cf
Douglas Nou <158287230+douglasnou@users.noreply.github.com>
2024-02-03 15:37:50 -0300

    Projeto "pedra, papel e tesoura" atualizado

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 /* Desenvolva seu código abaixo */
 
 
-function playRockPaperScissor (player1, player2) {
-    const jogador1 = player1;
-    const jogador2 = player2;
-    let pedra = 'Pedra';
-    let papel = 'Papel';
-    let tesoura = 'Tesoura';
-
-    if(jogador1 == 'Pedra' && jogador2 =='Papel'||
-    jogador1 == 'Tesoura' && jogador2 == 'Pedra'||
-    jogador1 == 'Papel' && jogador2 == 'Tesoura') {
+function playRockPaperScissor (jogador1, jogador2) {
+    
+    if(((jogador1 == 'Pedra') && (jogador2 =='Papel'))||
+    ((jogador1 == 'Tesoura') && (jogador2 == 'Pedra'))||
+    ((jogador1 == 'Papel') && (jogador2 == 'Tesoura'))) {
         return 'Jogador 2 venceu!'
-    }else if(jogador1 == 'Pedra' && jogador2 == 'Tesoura'||
-    jogador1 == 'Papel' && jogador2 == 'Pedra'||
-    jogador1 == 'Tesoura' && jogador2 == 'Papel') {
+    }else if(((jogador1 == 'Pedra') && (jogador2 == 'Tesoura'))||
+    ((jogador1 == 'Papel') && (jogador2 == 'Pedra'))||
+    ((jogador1 == 'Tesoura') && (jogador2 == 'Papel'))) {
         return 'Jogador 1 venceu!'
-    }else if(jogador1 == 'Pedra' && jogador2 == 'Pedra'||
-    jogador1 == 'Papel' && jogador2 == 'Papel'||
-    jogador1 == 'Tesoura' && jogador2 == 'Tesoura') {
+    }
         return 'Empate!'
     }
-}
+
 console.log(playRockPaperScissor('',''));
